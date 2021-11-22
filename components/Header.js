4aa533275d1f4ef3ddd32cf69b47441d59ca5855
Aminul Islam <aminulds61@gmail.com>
2021-11-22 17:50:07 +0600

    header done

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-[#203040] text-[#fff] p-5 flex">
+      <div className="text-lg cursor-pointer">E-Shop</div>
+    </header>
+  );
+}
+
+export default Header;
